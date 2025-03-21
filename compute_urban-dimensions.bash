407a456d97c0c9d1ac1ccfb1bd221bfadfc20487
Lluis Fita Borrell <lluis@debian-BULLSEYE-live-builder-AMD64>
2025-03-21 10:52:33 -0300

    Adding recomendation for num_urban_nz

--- compute_urban-dimensions.bash
+++ compute_urban-dimensions.bash
@@ -6,7 +6,7 @@
 num_urban=11
 
 # amount of vertical layers used by the urban module BEP
-#  NOTE: make sure that num_urban_nz * dz_urban >= 
+#  NOTE: make sure that num_urban_nz * dz_urban > highest building 
 num_urban_nz=6
 
 # amount of types of buildings per LCZ used by the urban module BEP
